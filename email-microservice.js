bb21258beb92eedf3bd05d5e4fe06b5089240ccb
rovemonteuxelkov <149589568+rovemonteuxelkov@users.noreply.github.com>
2023-12-07 12:09:35 +0100

    added API key security

---
+++ email-microservice.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const emailerModule = require('./elkov-messaging-engine/emailer-module');
+const loggingModule = require('./elkov-logging-engine/logging-module');
+const securityModule = require('./elkov-security-engine/security-module');
+
+const app = express();
+const port = 10000;
+app.use(express.json({limit: process.env.PAYLOAD_SIZE_LIMIT}));
+app.use(express.urlencoded({limit: process.env.PAYLOAD_SIZE_LIMIT}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({limit: process.env.PAYLOAD_SIZE_LIMIT}));
+app.use(bodyParser.urlencoded({limit: process.env.PAYLOAD_SIZE_LIMIT, extended: true}));
+
+async function callDefaultMap(req, res) {
+    try {
+    res.json("{ status: success }");
+    }
+   catch (error) {
+    res.status(500).json(error);
+  }
+  }
+
+app.post('/', (req, res) => {
+    securityModule.emailKeyAuthorization(req, res);
+    callDefaultMap(req, res);
+  });
+
+  app.listen(port, () => {
+    console.log(`ELKOV Email Microservice is running on port ${port}`);
+  });
---
+++ update_submodules.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git pull
+git submodule init
+git submodule
+git submodule update --init --recursive
+git submodule update --remote --recursive
