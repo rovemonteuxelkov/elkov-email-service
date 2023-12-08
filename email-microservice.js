eda8cddade96971b4d7799820b5b7831a2253214
rovemonteuxelkov <149589568+rovemonteuxelkov@users.noreply.github.com>
2023-12-08 13:39:54 +0100

    added email sending functionality

--- email-microservice.js
+++ email-microservice.js
@@ -8,43 +8,44 @@ const securityModule = require('./elkov-security-engine/security-module');
 
 const app = express();
 const port = 10000;
-app.use(express.json({limit: process.env.PAYLOAD_SIZE_LIMIT}));
-app.use(express.urlencoded({limit: process.env.PAYLOAD_SIZE_LIMIT}));
+app.use(express.json({ limit: process.env.PAYLOAD_SIZE_LIMIT }));
+app.use(express.urlencoded({ limit: process.env.PAYLOAD_SIZE_LIMIT }));
 
 app.use(bodyParser.json());
-app.use(bodyParser.json({limit: process.env.PAYLOAD_SIZE_LIMIT}));
-app.use(bodyParser.urlencoded({limit: process.env.PAYLOAD_SIZE_LIMIT, extended: true}));
+app.use(bodyParser.json({ limit: process.env.PAYLOAD_SIZE_LIMIT }));
+app.use(bodyParser.urlencoded({ limit: process.env.PAYLOAD_SIZE_LIMIT, extended: true }));
 
 async function callDefaultMap(req, res) {
-    try {
-    res.json("{ status: success }");
-    }
-   catch (error) {
+  try {
+    emailerModule.emailSendWithoutAttachment(req.body.to, req.body.subject, req.body.content);
+    res.json("{ status: ok }");
+  }
+  catch (error) {
     res.status(500).json(error);
   }
-  }
+}
 
 app.post('/', (req, res) => {
-    securityModule.emailKeyAuthorization(req, res);
-    callDefaultMap(req, res);
-  });
+  securityModule.emailKeyAuthorization(req, res);
+  callDefaultMap(req, res);
+});
 
-  // GET so Health Check are happy
+// GET so Health Check are happy
 app.get('/', (req, res) => {
-	try { 
-		res.json({ status: "healthy" });
-	}
-	catch (error) {
-		res.status(200).json(error);
-	}
+  try {
+    res.json({ status: "healthy" });
+  }
+  catch (error) {
+    res.status(200).json(error);
+  }
 });
 //
 
-  app.listen(port, () => {
-    console.log(`ELKOV Email Microservice is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`ELKOV Email Microservice is running on port ${port}`);
+});
 
-  app.use((req, res, next) => {
-    res.setHeader('X-Powered-By', 'ELKOV');
-    next();
-  });
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', 'ELKOV');
+  next();
+});
